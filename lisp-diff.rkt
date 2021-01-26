e9bb819ad6b606ff87c203fd07d11095cc108d1e
Tristan MacKinlay <tsmackinlay@gmail.com>
2021-01-26 00:13:07 -0800

    cleaned up

--- lisp-diff.rkt
+++ lisp-diff.rkt
@@ -2,12 +2,8 @@
 #lang racket
 (require rackunit)
 (require plai/datatype)
-(require)
-(provide (all-defined-out))
-
-(define (or? . preds)
-  (foldr (λ (acc pred) (λ (x) (or (acc x) (pred x)))) (λ (_) false) preds))
-
+(provide (rename-out [display-diff display-lisp-diff])
+                     )
 
 ;; Constants
 (define OUTPUT-GREEN "\e[32m")
@@ -17,63 +13,65 @@
 (define OUTPUT-YELLOW "\u001b[33m")
 (define RESET-OUTPUT-COLOR "\e[0m")
 
+;; predicate
+;; checks whether a value is valid as contents of list-diff
 (define (list-diff-contents? contents)
   (and (list? contents)
        (andmap (λ (el)
                  (or (list-diff? el)
                      (pair? el)
-                     (SAME*? el)
-                     (DIFFERENT*? el)))
+                     (partial? el)))
                contents)))
 
-;; s-expr is one-of:
-;; the empty list: '()
-;; a symbol
-;; (cons atom s-expr)
-
+;; representation of expression with differences highlighted
 (define-type diff-output
-  [SAME* (vals list?)]
   [SAME (val any/c)]
-  #;[SAME-VALUES (val list?)]
-  ;; diff-marker
-  [DIFFERENT* (left list?) (right list?)]
   [DIFFERENT (left any/c) (right any/c)]
   ;; diff-output can also be (listof diff-output)
   [list-diff (diffs list-diff-contents?)]
-  ;; could also be:
+  ;; could also be a pair:
   ;; (cons diff-output diff-output)
   )
 
-;; diff and compare-lists are mutually recursive.
+;; represent portion of a list containing sub-expressions
+;; to get same color.
+;; Should only appear inside 'contents field of lisp-diff
+(define-type partial
+  [SAME* (vals list?)]
+  [DIFFERENT* (left list?) (right list?)]
+  )
 
 ;; s-expr s-expr -> diff-output
-(define (diff left right) 
+;; return diff-output representing differences
+;; compare-expressions and compare-lists are mutually recursive.
+(define (compare-expressions left right) 
   (match `(,left . ,right)
     [`(,same . ,same) (SAME same)]
     [pair-of-lists #:when (and (list? left) (list? right))
                    (compare-lists left right)]
     [`((,lcar . ,lcdr) . (,rcar . ,rcdr))
-     (cons (diff lcar rcar) (diff lcdr rcdr))]
+     (cons (compare-expressions lcar rcar) (compare-expressions lcdr rcdr))]
     [else
      (DIFFERENT left right)]))
 
+;; list -> list-diff
 (define (compare-lists left right)
   ;; list -> (list-of diff-output)
   (define (compare-partials left right)
     (match `(,left . ,right)
       [`(() . ()) '()]
-      ;; pair
+      ;; pair which is not a list
       [`((,l1 . ,l2) (,r1 . r2))
        #:when (and (not (list? left))
                    (not (list? right)))
-       `(,(diff l1 r1)
+       `(,(compare-expressions l1 r1)
          . (diff l2 r2))]
       ;; lists begin with some matching values
       [`((,lheads ..1 ,ltail ...) . (,rheads ..1 ,rtail ...))
        #:when (equal? lheads rheads)
        (cons (SAME* lheads)
              (compare-partials ltail rtail))]
-      ;; lists starts with (unequal) lists
+      ;; lists both have a list as first element
       [`((,lhead ,ltail ...) . (,rhead ,rtail ...))
        #:when (and (list? lhead)
                    (list? rhead))
@@ -102,29 +100,29 @@
 
 (module+ test
   ;; pair handling
-  (check-equal? (diff '(siskel . ebert)
+  (check-equal? (compare-expressions '(siskel . ebert)
                       '(ebert . roeper))
                 `(,(DIFFERENT 'siskel 'ebert) . ,(DIFFERENT 'ebert 'roeper)))
-  (check-equal? (diff '() '()) (SAME '()))
-  (check-equal? (diff '() '(bless you)) (list-diff
+  (check-equal? (compare-expressions '() '()) (SAME '()))
+  (check-equal? (compare-expressions '() '(bless you)) (list-diff
                                          (list (DIFFERENT* '() '(bless you)))))
-  (check-equal? (diff '(right is empty) '()) (list-diff
+  (check-equal? (compare-expressions '(right is empty) '()) (list-diff
                                               (list (DIFFERENT* '(right is empty) '()))))
 
-  (check-equal? (diff '[dreamcast xbox-one ps4 switch]
+  (check-equal? (compare-expressions '[dreamcast xbox-one ps4 switch]
                       '[dreamcast xbox-one playstation])
                 (list-diff
                  [list (SAME* '(dreamcast xbox-one))
                        (DIFFERENT* '(ps4) '(playstation))
                        (DIFFERENT* '(switch) '())]))
-  (check-equal? (diff '([consoles [dreamcast xbox-one ps4 switch]])
+  (check-equal? (compare-expressions '([consoles [dreamcast xbox-one ps4 switch]])
                       '([consoles [dreamcast xbox-one playstation]]))
                 (list-diff `(,(list-diff `[,(SAME* '(consoles))
                                            ,(list-diff
                                              [list (SAME* '(dreamcast xbox-one))
                                                    (DIFFERENT* '(ps4) '(playstation))
                                                    (DIFFERENT* '(switch) '())])]))))
-  (check-equal? (diff
+  (check-equal? (compare-expressions
                  '(info
                    ((players
                      [(player "brandon" 20 180)
@@ -152,53 +150,13 @@
                           (DIFFERENT* '(40) '(400))
                           (SAME* '(204))))
                         (SAME* '(("maxwell" 31 150)))]))))))))
-  (check-equal? (diff '(same (nested list)) '(same (with different contents)))
+  (check-equal? (compare-expressions '(same (nested list)) '(same (with different contents)))
                 (list-diff
                  (list
                   (SAME* '(same))
                   (DIFFERENT* '((nested list)) '((with different contents))))))
   )
 
-;; list-of diff-output -> list-of diff-output
-;; merge adjacent OKs and HEREs
-(define (merge-diffs ls)
-  (match ls
-    ['() '()]
-    [`(,(SAME* oks) ..1 ,tail ...)
-     `(,(SAME* (foldr append '() oks))
-       ,@(merge-diffs tail))]
-    [(list (DIFFERENT* `(,ls) `(,rs)) ..1 tail ... )
-     `(,(DIFFERENT* ls rs) ,@(merge-diffs tail))]
-    ))
-  
-
-(module+ test
-  (check-equal? (merge-diffs (list (SAME* '(there)) (SAME* '(are)) (SAME* '(four)) (DIFFERENT* '(waffles) '(lights))))
-                (list (SAME* '(there are four)) (DIFFERENT* '(waffles) '(lights)))))
-
-
-;; used to repeat a string n times as a new string.
-;; like the * method in ruby.
-(define (*str s n)
-  (cond
-    [(<= n 0) ""]
-    [else
-     (string-append s (*str s (- n 1)))]
-    )
-  )
-
-(define SPACE " ")
-(define INDENT-CHAR SPACE)
-(define INDENT-WIDTH 2)
-(define INDENT (*str INDENT-CHAR INDENT-WIDTH))
-
-;; number string -> string
-(define (indent-to indent-depth str)
-  (let ([lines (string-split str "\n")])
-    (string-join (map (λ (s) (string-append (*str INDENT indent-depth)
-                                            s))
-                      lines)
-                 "\n")))
 ;; diff-output -> string
 (define (colorize-diff tree)
   ;; partial -> list
@@ -248,7 +206,8 @@
       "\n")
   )
 
-(define show-diff (compose displayln colorize-diff diff))
+(define display-diff (compose displayln colorize-diff compare-expressions))
+(define lisp-diff (compose colorize-diff compare-expressions))
 
 (module+ test
   (check-equal? (colorize-diff
@@ -265,26 +224,26 @@
                      )
                     RESET-OUTPUT-COLOR
                     "\n"))
-  (show-diff  `(same
+  (display-diff  `(same
                 (same))
               `(same
                 (same (but different))))
 
-  (show-diff '(first (second)
+  (display-diff '(first (second)
                      (third something))
              '(first (second)
                      (third (something-else))))
 
-  (show-diff '(same up to here then (list with multiple differences))
+  (display-diff '(same up to here then (list with multiple differences))
              '(same up to here then (and different length)))
-  (show-diff '(when the head is different and the tail is the same)
+  (display-diff '(when the head is different and the tail is the same)
              '(but the head is different and the tail is the same))
-  (show-diff 'single-symbol 'comparison)
-  (show-diff '(we . compare)'(a . pair))
-  (show-diff '(second-item . matches) '(pair-cdr . matches))
+  (display-diff 'single-symbol 'comparison)
+  (display-diff '(we . compare)'(a . pair))
+  (display-diff '(second-item . matches) '(pair-cdr . matches))
   ;; complex structure with a small difference higher up, then another difference lower down
-  (show-diff '(same (nested list)) '(same (with different contents)))
-  (show-diff
+  (display-diff '(same (nested list)) '(same (with different contents)))
+  (display-diff
    '(info
      ((players
        [(player "brandon" 20 180) (player "kevin" 40 204) ("maxwell" 31 150)])))
